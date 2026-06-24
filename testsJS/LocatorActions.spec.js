8ec9950bb2625f7040655faceb9ff31925a434c2
learnautomation1604-cyber <learnautomation1604@gmail.com>
2026-06-24 08:26:24 +0530

    Commitfor_LocatorActions_24062026

--- testsJS/LocatorActions.spec.js
+++ testsJS/LocatorActions.spec.js
@@ -26,16 +26,51 @@ test("Checkbox", async({page}) => {
 
     await page.goto("https://testautomationpractice.blogspot.com/");
     await page.locator('input#male').scrollIntoViewIfNeeded();
+    await page.waitForTimeout(3000);
+    await page.locator('input#sunday').check();
+    await page.waitForTimeout(3000);
+    await page.locator('input#sunday').uncheck();
+    await page.waitForTimeout(3000);
+    // locator.check() - Checkbox check
+
+});
+
+
+test("hover, Highlight", async({page}) => {
+
+    await page.goto("https://www.ebay.com");
 
     await page.waitForTimeout(3000);
+    await page.locator("(//div[@class='gh-flyout is-right-aligned gh-flyout--icon-target'])[2]").hover();
+    await page.locator("(//div[@class='gh-flyout is-right-aligned gh-flyout--icon-target'])[2]").highlight();
 
-    await page.getByText('Test') // tagname.classvalue, tagname#idvalue
-
-
+    await page.waitForTimeout(3000);
 
     // locator.check() - Checkbox check
 
 });
+
+
+test("fill Value", async({page}) => {
+
+    await page.goto("https://www.ebay.com");
+    await page.waitForTimeout(3000);
+    await page.locator("input#gh-ac").fill("i Phone 16 Pro");
+    await page.waitForTimeout(3000);
+    await page.locator("//button[@class='gh-search-button btn btn--primary']").click();
+    await page.waitForTimeout(3000);
+
+    // locator.check() - Checkbox check
+
+});
+
+// press(Enter), Press(Down)
+// tap()
+//focus()
+// blur()
+// SelectOption(values)
+// selectInputFields(file)
+
 
 
 // id, name, class, text, class name, css selector, link text, partial link text, x path 
