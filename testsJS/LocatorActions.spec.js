6426bc8326c1ed2dc5d0a0008010ff7a654c5ce4
learnautomation1604-cyber <learnautomation1604@gmail.com>
2026-06-27 09:18:59 +0530

    commit_Locator_06272026

--- testsJS/LocatorActions.spec.js
+++ testsJS/LocatorActions.spec.js
@@ -1,71 +1,113 @@
 import {test} from "@playwright/test"
 
-test("LocatorClick", async ({page}) => {
+// test("LocatorClick", async ({page}) => {
+//     await page.goto("https://www.ebay.com");
+//     // locator.click
+//     await page.locator("//*[text()='Shop by category']").click();
+//     await page.locator("//*[text()='Shop by category']").click();
+// });
 
+
+// test("Doubleclick", async({page}) => {
+//     await page.goto("https://www.ebay.com");
+//     // locator. boubleclick()
+//     await page.locator("//*[text()='Shop by category']").dblclick();
+
+//     // locator.check() - Checkbox check
+// });
+
+// test("Checkbox", async({page}) => {
+
+//     await page.goto("https://testautomationpractice.blogspot.com/");
+//     await page.locator('input#male').scrollIntoViewIfNeeded();
+//     await page.waitForTimeout(3000);
+//     await page.locator('input#sunday').check();
+//     await page.waitForTimeout(3000);
+//     await page.locator('input#sunday').uncheck();
+//     await page.waitForTimeout(3000);
+//     // locator.check() - Checkbox check
+
+// });
+
+
+// test("hover, Highlight", async({page}) => {
+
+//     await page.goto("https://www.ebay.com");
+
+//     await page.waitForTimeout(3000);
+//     await page.locator("(//div[@class='gh-flyout is-right-aligned gh-flyout--icon-target'])[2]").hover();
+//     await page.locator("(//div[@class='gh-flyout is-right-aligned gh-flyout--icon-target'])[2]").highlight();
+
+//     await page.waitForTimeout(3000);
+
+//     // locator.check() - Checkbox check
+
+// });
+
+
+// test("fill Value", async({page}) => {
+//     await page.goto("https://www.ebay.com");
+//     await page.waitForTimeout(3000);
+//     await page.locator("input#gh-ac").fill("i Phone 16 Pro");
+//     await page.waitForTimeout(3000);
+//     await page.locator("//button[@class='gh-search-button btn btn--primary']").click();
+//     await page.waitForTimeout(3000);
+//     // locator.check() - Checkbox check
+// });
+
+// test("press action", async({page}) => {
+//     await page.goto("https://www.ebay.com");
+//     await page.waitForTimeout(3000);
+//     await page.locator("input#gh-ac").fill("i Phone 16 Pro");
+//     await page.waitForTimeout(3000);
+//     await page.keyboard.press('Enter');
+//     await page.waitForTimeout(3000);
+//     await page.locator("//span[@class='srp-format-tabs-h2'][normalize-space()='Buy It Now']").press('Enter');
+//     await page.waitForTimeout(5000);
+// })
+
+// test("tap Action", async({page}) =>{
+//     await page.goto("https://www.ebay.com");
+//     await page.waitForTimeout(6000);
+//     await page.locator("input#gh-ac").fill("i Phone 16 Pro");
+//     await page.waitForTimeout(3000);
+//     await page.keyboard.press('Tab')
+//     await page.waitForTimeout(3000);
+
+// });
+// test("focus Action", async({page}) =>{
+//     await page.goto("https://www.ebay.com");
+//     await page.waitForTimeout(6000);
+//     await page.locator("input#gh-ac").fill("i Phone 16 Pro");
+//     await page.waitForTimeout(3000);
+//     await page.locator("//button[@id='gh-search-btn']").focus();
+//     await page.waitForTimeout(3000);
+//     await page.locator("//button[@id='gh-search-btn']").blur();
+//     await page.waitForTimeout(3000);
+//     await page.locator("//button[@id='gh-search-btn']").highlight();
+
+//     await page.waitForTimeout(6000);
+
+
+// });
+
+test("Drop Down Handling", async({page}) =>{
     await page.goto("https://www.ebay.com");
-
-    // locator.click
-
-    await page.locator("//*[text()='Shop by category']").click();
-    await page.locator("//*[text()='Shop by category']").click();
+    await page.waitForTimeout(6000);
+    await page.locator("//select[@id='gh-cat']").selectOption("Art");
+    await page.waitForTimeout(3000);
+    await page.locator("//select[@id='gh-cat']").selectOption(267);
+    await page.waitForTimeout(3000);
 
 
 });
 
 
-test("Doubleclick", async({page}) => {
-
-    await page.goto("https://www.ebay.com");
-    // locator. boubleclick()
-    await page.locator("//*[text()='Shop by category']").dblclick();
-
-    // locator.check() - Checkbox check
-});
-
-test("Checkbox", async({page}) => {
-
-    await page.goto("https://testautomationpractice.blogspot.com/");
-    await page.locator('input#male').scrollIntoViewIfNeeded();
-    await page.waitForTimeout(3000);
-    await page.locator('input#sunday').check();
-    await page.waitForTimeout(3000);
-    await page.locator('input#sunday').uncheck();
-    await page.waitForTimeout(3000);
-    // locator.check() - Checkbox check
-
-});
 
 
-test("hover, Highlight", async({page}) => {
-
-    await page.goto("https://www.ebay.com");
-
-    await page.waitForTimeout(3000);
-    await page.locator("(//div[@class='gh-flyout is-right-aligned gh-flyout--icon-target'])[2]").hover();
-    await page.locator("(//div[@class='gh-flyout is-right-aligned gh-flyout--icon-target'])[2]").highlight();
-
-    await page.waitForTimeout(3000);
-
-    // locator.check() - Checkbox check
-
-});
 
 
-test("fill Value", async({page}) => {
-
-    await page.goto("https://www.ebay.com");
-    await page.waitForTimeout(3000);
-    await page.locator("input#gh-ac").fill("i Phone 16 Pro");
-    await page.waitForTimeout(3000);
-    await page.locator("//button[@class='gh-search-button btn btn--primary']").click();
-    await page.waitForTimeout(3000);
-
-    // locator.check() - Checkbox check
-
-});
-
-// press(Enter), Press(Down)
-// tap()
+// press('Enter'), Press('Down'), press('Tab')
 //focus()
 // blur()
 // SelectOption(values)
